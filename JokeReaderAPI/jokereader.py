affdb09a542e8e015357d45636fd11d27b95ae8a
Nicholas Yek <69704055+nichyjt@users.noreply.github.com>
2020-12-22 15:16:28 +0800

    Add files via upload
    add setting class structure to properly handle setting param persistence

--- JokeReaderAPI/jokereader.py
+++ JokeReaderAPI/jokereader.py
@@ -3,17 +3,31 @@ import requests as req
 # Get a joke from jokeapi and print to console.
 # Not using 3rd party api wrappers.
 
-# Setting Variables
+# Settings Constants
 JOKE_CAT_PROGRAMMING = 'Programming'
 JOKE_CAT_DARK = 'Dark'
 JOKE_CAT_PUN = 'Pun'
 JOKE_CAT_SPOOKY = 'Spooky'
 JOKE_CAT_CHRISTMAS = 'Christmas'
-nsfw = False
-religious = False
-political = False
-racist = False
-sexist = False
+
+# Settings class to handle persistent flags
+class settings():
+    def __init__(self):
+        self.nsfw = False
+        self.religious = False
+        self.political = False
+        self.racist = False
+        self.sexist = False
+    def getSettings(self):
+        string = "Joke settings: \n"
+        string += "No NSFW: "+str(self.nsfw)+'\n'
+        string += "No Religious: " + str(self.religious)+'\n'
+        string += "No Political: "+str(self.political)+'\n'
+        string += "No Racist: "+str(self.racist)+'\n'
+        string += "No Sexist: "+str(self.sexist)+'\n'
+        return string
+
+jokeSettings = settings()
 
 # jokeapi endpoint https://sv443.net/jokeapi/v2/joke/[Category/-ies]
 def quickJoke():
@@ -45,32 +59,34 @@ def getJokeSpecific(id=0):
         print(joke['delivery'])
     print("This is joke number: "+str(joke['id']))
 
-def setJokeFilters(no_nsfw=nsfw, no_religious=religious, no_political=political, no_racist=racist, no_sexist=sexist):
-    nsfw = no_nsfw
-    religious = no_religious
-    political = no_political
-    racist = no_racist
-    sexist = no_sexist
+def setJokeFilters(no_nsfw=jokeSettings.nsfw, no_religious=jokeSettings.religious, no_political=jokeSettings.political, no_racist=jokeSettings.racist, no_sexist=jokeSettings.sexist):
+    jokeSettings.nsfw = no_nsfw
+    jokeSettings.religious = no_religious
+    jokeSettings.political = no_political
+    jokeSettings.racist = no_racist
+    jokeSettings.sexist = no_sexist
+    print(jokeSettings.getSettings())
 
 def getJoke(category='any'):
     url = "https://sv443.net/jokeapi/v2/joke"
     url += "/"+category
     numFlags = 0
     blacklist = "?blacklistflags="
-    if nsfw:
-        ++numFlags
+
+    if jokeSettings.nsfw:
+        numFlags+=1
         blacklist += 'nsfw'
-    if religious:
-        ++numFlags
+    if jokeSettings.religious:
+        numFlags+=1
         blacklist += ',religious' if numFlags>1 else 'religious'
-    if political:
-        ++numFlags
+    if jokeSettings.political:
+        numFlags+=1
         blacklist += ',political' if numFlags>1 else 'political'
-    if racist:
-        ++numFlags
+    if jokeSettings.racist:
+        numFlags+=1
         blacklist += ',racist' if numFlags>1 else 'racist'
-    if sexist:
-        ++numFlags
+    if jokeSettings.sexist:
+        numFlags+=1
         blacklist += ',sexist' if numFlags>1 else 'sexist'
     if numFlags !=0:
         url+=blacklist
@@ -79,9 +95,9 @@ def getJoke(category='any'):
     if joke['error']:
         print("HTTP ERROR "+str(joke['code']))
         return
+    print("\nThis is joke number: "+str(joke['id'])+"\n")
     if joke['type']=='single':
-        print(joke["joke"])
+        print(joke["joke"]+'\n')
     else:
         print(joke['setup'])
-        print(joke['delivery'])
-    print("This is joke number: "+str(joke['id']))
+        print(joke['delivery']+'\n')
--- JokeReaderAPI/jokereaderexample.py
+++ JokeReaderAPI/jokereaderexample.py
@@ -3,5 +3,5 @@ import jokereader as joke
 # Debug script
 # Simulating a bored programmer using jokereader.py
 
-joke.setJokeFilters(True,True,False,False,True)
-joke.getJoke(joke.JOKE_CAT_CHRISTMAS)
+joke.setJokeFilters(no_nsfw=True, no_political=True)
+joke.getJoke(joke.JOKE_CAT_PROGRAMMING)
